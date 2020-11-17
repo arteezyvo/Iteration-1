dc0d620e06bc2cd1233df728c550dea9da3326a5
arteezyvo <56277195+arteezyvo@users.noreply.github.com>
2020-11-17 11:11:33 -0600

    Add files via upload

---
+++ RapidContactTracer/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RapidContactTracer.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        public ActionResult Testing()
+        {
+            return View();
+        }
+
+        public ActionResult SeatingChart()
+        {
+            return View();
+        }
+    }
+}
